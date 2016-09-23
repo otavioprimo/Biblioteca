400e4acf743cf084fd60e8d797aa98b204eb046b
Usuário <Usurio@192.168.15.31>
2016-09-23 11:26:41 -0300

    Otávio 23/09

--- src/Util/Criptografia.java
+++ src/Util/Criptografia.java
@@ -1,4 +1,4 @@
-package Resources;
+package Util;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
--- src/Util/EnviarEmail.java
+++ src/Util/EnviarEmail.java
@@ -1,4 +1,4 @@
-package Resources;
+package Util;
 
 import java.util.Properties;
 import javax.mail.Address;
