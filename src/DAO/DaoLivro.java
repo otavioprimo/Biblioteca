3f1086b3642fe24aea52ff0a783429cb9f40028b
Otavio Augusto Primo <otavioprimo@gmail.com>
2016-11-08 19:46:18 -0200

    Update DaoLivro.java

--- src/DAO/DaoLivro.java
+++ src/DAO/DaoLivro.java
@@ -7,6 +7,9 @@ package DAO;
 
 import Conexao.ConnectionFactory;
 import Modelo.Livro;
+import Modelo.Autor;
+import Modelo.Editora;
+import Modelo.Genero;
 import java.sql.CallableStatement;
 import java.sql.SQLException;
 
@@ -49,7 +52,67 @@ public class DaoLivro {
         connOracle.desconectar();
     }
 
-    public void editar(Livro livro) throws SQLException {
+    public void editar(Livro livro, String nomeAnterior, int idPar) throws SQLException {
+        connOracle.conectar();
+        CallableStatement cs;
         
+        cs = connOracle.conn.prepareCall("BEGIN UPDT(UPPER(?),?,?,TO_TIMESTAMP(?,'DD/MM/YYYY'),UPPER(?),?,?,UPPER(?),?,UPPER(?),?,?); END;");
+        cs.setString(1, livro.getTitulo());
+        cs.setInt(2, livro.getAutor().getIdAutor());
+        cs.setInt(3, livro.getNum_pag());
+        cs.setString(4, livro.getDt_lanc());
+        cs.setString(5, livro.getEdicao());
+        cs.setString(6, livro.getIsbn());
+        cs.setInt(7, livro.getEditora().getIdEditora());
+        cs.setString(8, livro.getPais());
+        cs.setInt(9, livro.getGenero().getIdGenero());
+        cs.setString(10, livro.getEstante());
+        cs.setString(11,nomeAnterior);
+        cs.setInt(12,idPar);                //Verificar se nome anterior é num 11 ou 12 se nao existir da alter na proc e botar nomeAnterior
+        cs.execute();
+        connOracle.desconectar();
+    }
+    
+    public ArrayList<Livro> listar() throws SQLException{
+        connOracle.conectar();
+
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT ..."); //Alterar na tabela livro para Nome_Autor, Nome_Editora
+        
+        PreparedStatement pst = connOracle.conn.prepareStatement(sql.toString());
+        ResultSet resultado = pst.executeQuery();
+
+        ArrayList<Livro> lista = new ArrayList<Cliente>();
+        while (resultado.next()) {
+            Autor autor = new Autor();
+            autor.setIdAutor(resultado.getInt("IDAutor"));
+            autor.setNome(resultado.getString("Nome_Autor"));
+            
+            Genero genero = new Genero();
+            genero.setIdGenero(resultado.getInt("IDGenero"));
+            genero.setNome(resultado.getString("Tipo"));
+            
+            Editora editora = new Editora();
+            editora.setIdEditora(resultad.getInt("IDEditora"));
+            editora.setTipo(resultado.getString("Nome_Editora"));
+            
+            Livro livro = new Livro();
+            livro.setIdLivro(resultado.getInt("IDLivro"));
+            livro.setTitulo(resultado.getString("Titulo"));
+            livro.setAutor(autor);
+            livro.setNum_pag(resultado.getInt("Num_Pag"));
+            livro.setDt_lanc(resultado.getString("Dt_Lanc"));
+            livro.setEdicao(resultado.getString("Edicao"));
+            livro.setIsbn(resultado.getString("ISBN"));
+            livro.setEditora(editora);
+            livro.setPais(resultado.getString("Pais"));
+            livro.setGenero(genero);
+            livro.setEstante(resultado.getString("Estante"));
+            
+            lista.add(Livro);
+        }
+        
+        connOracle.desconectar();
+        return lista;
     }
 }
